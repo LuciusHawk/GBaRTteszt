078f102796c1438b93a0268aae620403ff28cf05
LuciusHawk <jeronimo.cpt@gmail.com>
2018-03-07 19:52:14 +0100

    symfony 4 init commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Helper\FormHelper;
+use App\Service\TableMaker;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+
+    /**
+     * @Route("/")
+     */
+    public function index(Request $request)
+    {
+        // creates a task and gives it some dummy data for this example
+        return $this->render('base.html.twig', array(
+
+        ));
+    }
+}
